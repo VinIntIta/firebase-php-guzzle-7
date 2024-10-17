d53d8f8bb8a387b290dabc8e545efb79047381ac
Seregamk <serega.mk2@gmail.com>
2024-10-17 18:30:26 +0300

    Updated deprecated Guzzle methods to the newer version ^7 +

--- src/Firebase/Messaging/Http/Request/SendMessage.php
+++ src/Firebase/Messaging/Http/Request/SendMessage.php
@@ -5,8 +5,7 @@ declare(strict_types=1);
 namespace Kreait\Firebase\Messaging\Http\Request;
 
 use GuzzleHttp\Psr7\Request;
-use function GuzzleHttp\Psr7\stream_for;
-use function GuzzleHttp\Psr7\uri_for;
+use GuzzleHttp\Psr7\Utils;
 use Kreait\Firebase\Http\WrappedPsr7Request;
 use Kreait\Firebase\Messaging\Message;
 use Psr\Http\Message\RequestInterface;
@@ -14,11 +13,11 @@ use Psr\Http\Message\RequestInterface;
 final class SendMessage implements RequestInterface
 {
     use WrappedPsr7Request;
-
+    
     public function __construct(string $projectId, Message $message)
     {
-        $uri = uri_for('https://fcm.googleapis.com/v1/projects/'.$projectId.'/messages:send');
-        $body = stream_for(\json_encode(['message' => $message]));
+        $uri = Utils::uriFor('https://fcm.googleapis.com/v1/projects/'.$projectId.'/messages:send');
+        $body = Utils::streamFor(\json_encode(['message' => $message]));
         $headers = [
             'Content-Type' => 'application/json; charset=UTF-8',
             'Content-Length' => $body->getSize(),
@@ -27,3 +26,4 @@ final class SendMessage implements RequestInterface
         $this->wrappedRequest = new Request('POST', $uri, $headers, $body);
     }
 }
+
